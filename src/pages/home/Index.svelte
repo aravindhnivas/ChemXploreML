8f4b0a3b975c9a1675da77e5f5af3e6a1b93ff76
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2025-03-28 23:55:17 -0400

    icon main page

--- src/pages/home/Index.svelte
+++ src/pages/home/Index.svelte
@@ -8,11 +8,13 @@
         <Pane class="p-2">
             <div class="hero h-full">
                 <div class="hero-content text-center">
-                    <div class="max-w-md">
+                    <div class="grid max-w-md gap-5 justify-items-center">
+                        <img src="/icons/icon.png" alt="ChemXploreML Logo" class="w-32 h-32 m-auto" />
+
                         <h1 class="text-5xl font-bold">ChemXploreML</h1>
-                        <p class="py-6">A general-purpose desktop application for molecular property prediction</p>
+                        <p>A general-purpose desktop application for chemistry</p>
                         <button
-                            class="btn btn-sm btn-outline"
+                            class="btn btn-sm btn-outline w-max"
                             on:click={() => {
                                 logger.info('Get Started button clicked');
                             }}>Get Started</button
