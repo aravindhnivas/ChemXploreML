5f7027e810f615deaa01815c5d09d61f0f1c8d77
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2025-04-04 16:02:04 -0400

    Update About.svelte

--- src/pages/settings/About.svelte
+++ src/pages/settings/About.svelte
@@ -20,31 +20,31 @@
     <h1>About</h1>
     <div class="grid grid-cols-5 w-[350px] gap-2">
         <div class="col-span-3">Platform</div>
-        <span class="col-span-2 badge badge-info">{system_info.platform}-{system_info.arch}</span>
+        <span class="col-span-2 badge badge-dark">{system_info.platform}-{system_info.arch}</span>
 
         <div class="col-span-3">RAM</div>
-        <span class="col-span-2 badge badge-info">{$RAM_SIZE.toFixed(0)} GB</span>
+        <span class="col-span-2 badge badge-dark">{$RAM_SIZE.toFixed(0)} GB</span>
 
         <div class="col-span-3">CPU</div>
-        <span class="col-span-2 badge badge-info">{$CPU_COUNT.toFixed(0)} core</span>
+        <span class="col-span-2 badge badge-dark">{$CPU_COUNT.toFixed(0)} core</span>
 
         <div class="col-span-5 divider"></div>
 
         <div class="col-span-3">ChemXploreML</div>
         {#await getVersion() then value}
-            <span class="col-span-2 badge badge-info">v{value}</span>
+            <span class="col-span-2 badge badge-dark">v{value}</span>
         {/await}
 
         <div class="col-span-3">Tauri</div>
         {#await getTauriVersion() then value}
-            <span class="col-span-2 badge badge-info">v{value}</span>
+            <span class="col-span-2 badge badge-dark">v{value}</span>
         {/await}
 
         <div class="col-span-3">Python</div>
-        <span class="col-span-2 badge badge-info" class:badge-error={!$pyVersion}>{$pyVersion || 'unknown'}</span>
+        <span class="col-span-2 badge badge-dark" class:badge-error={!$pyVersion}>{$pyVersion || 'unknown'}</span>
 
         <div class="col-span-3">umdapy</div>
-        <span class="col-span-2 badge badge-info" class:badge-error={!$umdapyVersion}
+        <span class="col-span-2 badge badge-dark" class:badge-error={!$umdapyVersion}
             >v{$umdapyVersion || 'unknown'}</span
         >
     </div>
