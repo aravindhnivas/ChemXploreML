8db3d909d3ee7a89c99f0243dbf2a45f0ff83af5
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2025-04-06 18:56:58 -0400

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -29,17 +29,18 @@ use std::fs;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(
-            tauri_plugin_log::Builder::new()
-            .level(log::LevelFilter::Info)
-            .target(tauri_plugin_log::Target::new(
-                        tauri_plugin_log::TargetKind::LogDir {
-                        file_name: Some("logs".to_string()),
-                        },
-                    ))
-            .max_file_size(50_000 /* bytes */)
-            .build()
-        )
+        // .plugin(
+        //     tauri_plugin_log::Builder::new()
+        //     .level(log::LevelFilter::Info)
+        //     .target(tauri_plugin_log::Target::new(
+        //                 tauri_plugin_log::TargetKind::LogDir {
+        //                 file_name: Some("ChemXploreML".to_string()),
+        //                 },
+        //             ))
+        //     .max_file_size(50_000 /* bytes */)
+        //     .build()
+        // )
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_os::init())
