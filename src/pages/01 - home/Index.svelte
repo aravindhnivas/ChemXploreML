39ef09a2330c9ed084f838b9a5bada2ce42d6ddc
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2025-04-08 19:26:27 -0400

    Update Index.svelte

--- src/pages/01 - home/Index.svelte
+++ src/pages/01 - home/Index.svelte
@@ -9,16 +9,22 @@
             <div class="hero h-full">
                 <div class="hero-content text-center">
                     <div class="grid max-w-md gap-5 justify-items-center">
-                        <img src="/icons/icon.png" alt="ChemXploreML Logo" class="w-32 h-32 m-auto" />
+                        <div class="animate__animated animate__fadeInDown">
+                            <img src="/icons/icon.png" alt="ChemXploreML Logo" class="w-32 h-32 m-auto" />
+                        </div>
 
-                        <h1 class="text-5xl font-bold">ChemXploreML</h1>
-                        <p>A general-purpose desktop application for chemistry</p>
-                        <button
-                            class="btn btn-sm btn-outline w-max"
-                            on:click={() => {
-                                logger.info('Get Started button clicked');
-                            }}>Get Started</button
-                        >
+                        <div class="animate__animated animate__fadeInUp">
+                            <h1 class="text-5xl font-bold">ChemXploreML</h1>
+                            <p>A general-purpose desktop application for chemistry</p>
+                        </div>
+                        <div class="animate__animated animate__fadeInUp">
+                            <button
+                                class="btn btn-sm btn-outline w-max"
+                                on:click={() => {
+                                    logger.info('Get Started button clicked');
+                                }}>Get Started</button
+                            >
+                        </div>
                     </div>
                 </div>
             </div>
