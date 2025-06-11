96e61d81ee53e5eb2e75c4b3849ec286d8f63395
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2025-06-11 09:49:27 -0400

    Update PredictionTable.svelte

--- src/pages/06 - ml_training/PredictionTable.svelte
+++ src/pages/06 - ml_training/PredictionTable.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { Trash2, Download } from 'lucide-svelte/icons';
+
     export let prediction_table_data: {
         id: string;
         smiles: string;
@@ -8,14 +10,33 @@
         prediction: string;
     }[];
     const columns = ['smiles', 'model', 'embedder', 'pre_trained_model', 'prediction'] as const;
+
     const clear_table = () => {
         prediction_table_data = [];
+    };
+
+    const download_table = async () => {
+        const data = prediction_table_data.map(row => Object.values(row).slice(1).join(',')).join('\n');
+        const filename = await dialog.save({
+            filters: [{ name: 'CSV', extensions: ['csv'] }],
+        });
+        if (!filename) return;
+        await fs.writeFile(filename, new TextEncoder().encode(data));
+        toast.success('Table downloaded');
     };
 </script>
 
 {#if prediction_table_data.length > 0}
     <div class="flex-gap w-full justify-end">
-        <button class="btn btn-sm btn-outline w-max" on:click={clear_table}> Clear table </button>
+        <button class="btn btn-sm btn-outline" on:click={download_table}>
+            <Download class="w-4 h-4" />
+            Download table
+        </button>
+
+        <button class="btn btn-sm btn-error" on:click={clear_table}>
+            <Trash2 class="w-4 h-4" />
+            Clear table
+        </button>
     </div>
 
     <div style="height: 400px; width: 100%;" class="overflow-auto grid gap-2 content-start">
